fb72867c07d2ad8b3335a51a091654d09a5bca27
jmtomas <jmtomaszafra@gmail.com>
2021-06-16 21:14:31 +0200

    bootloader

--- js/serialports.js
+++ js/serialports.js
@@ -7,28 +7,40 @@ let previous_ports;
 async function listSerialPorts() {
     await serialports.list().then((ports, err) => {
         let total = document.getElementById('total_ports');
+        let total_boot = document.getElementById('total_ports_toboot');
         if(err) {
             document.getElementById('error').textContent = err.message
+            document.getElementById('error_boot').textContent = err.message
             total.innerHTML = '';
+            total_boot.innerHTML = '';
             return
         } else {
             document.getElementById('error').textContent = ''
+            document.getElementById('error_boot').textContent = ''
         }
         //console.log('ports', ports);
 
         if (ports.length === 0) {
             document.getElementById('error').textContent = 'No ports discovered'
+            document.getElementById('error_boot').textContent = 'No ports discovered'
         }
         let list = document.getElementById('ports');
+        let list_boot = document.getElementById('ports_toboot');
         if (previous_ports !== ports.length) {
             total.innerHTML = 'Ports detected: <b>' + ports.length + '</b>';
+            total_boot.innerHTML = 'Ports detected: <b>' + ports.length + '</b>';
             list.innerHTML = '<div>';
+            list_boot.innerHTML = '<div>';
             Object.entries(ports).forEach(([key, value]) => {
                 list.innerHTML += '<div class="field">' +
                     '<input id="'+ value.path + '" type="checkbox" name="port" class="switch" checked="checked">' +
                     '<label for="'+ value.path + '">'+ value.path + '</label></div>';
+                list_boot.innerHTML += '<div class="field">' +
+                    '<input id="'+ value.path + '" type="checkbox" name="port_boot" class="switch" checked="checked">' +
+                    '<label for="'+ value.path + '">'+ value.path + '</label></div>';
             });
             list.innerHTML += '</div>';
+            list_boot.innerHTML += '</div>';
         }
         previous_ports = ports.length;
     })
@@ -40,18 +52,29 @@ setTimeout(function listPorts() {
 }, 2000);
 
 
-function manageMinerConfig () {
+function manageMinerConfig (method) {
     let username = document.getElementById('username').textContent;
     let avrport = [];
     let dir = './AVRMiner_2.49_resources';
     if (!fs.existsSync(dir)){
         fs.mkdirSync(dir);
     }
-    document.querySelectorAll("input[type=checkbox][name=port]:checked").forEach(function(port){
-        avrport.push(port.id);
-    });
+    if (method === 'mining') {
+        document.querySelectorAll("input[type=checkbox][name=port]:checked").forEach(function(port){
+            avrport.push(port.id);
+        });
+    }
+    if (method === 'boot') {
+        document.querySelectorAll("input[type=checkbox][name=port_boot]:checked").forEach(function(port){
+            avrport.push(port.id);
+        });
+    }
     let identifier_string = document.getElementById("identifier").value;
     let identifier = identifier_string !== '' ? identifier_string : 'none';
+    let avrboard = '';
+    if (method === 'boot') {
+        avrboard = "avrboard = " + document.getElementById('avr_board').value;
+    }
     let content = "[arduminer]\n" +
         "username = " + username + "\n" +
         "avrport = " + avrport.toString() + "\n" +
@@ -59,6 +82,7 @@ function manageMinerConfig () {
         "language = english\n" +
         "identifier = " + identifier + "\n" +
         "debug = n\n" +
+        avrboard +
         "\n";
     fs.writeFile(dir + '/Miner_config.cfg', content, (err) => {
         if(err){
@@ -73,7 +97,12 @@ function manageMinerConfig () {
                 modal_error.classList.remove('is-active');
             }
         } else {
-            runMiner();
+            if (method === 'mining') {
+                runMiner();
+            }
+            if (method === 'boot') {
+                runBootloader();
+            }
         }
     });
 }
@@ -81,14 +110,22 @@ function manageMinerConfig () {
 window.addEventListener('load', function() {
     let start_mining = document.getElementById('start_mining');
     start_mining.onclick = function () {
-        manageMinerConfig();
+        manageMinerConfig('mining');
         start_mining.setAttribute("disabled", true);
     };
     let clear_console = document.getElementById('clear_console');
     clear_console.onclick = function () {
         document.getElementById('traces').innerHTML = '';
     };
+    let clear_console_boot = document.getElementById('clear_console_boot');
+    clear_console_boot.onclick = function () {
+        document.getElementById('traces_boot').innerHTML = '';
+    };
 
+    let start_boot = document.getElementById('start_boot');
+    start_boot.onclick = function () {
+        manageMinerConfig('boot');
+    }
 });
 
 function runMiner() {
@@ -113,9 +150,32 @@ function runMiner() {
         traces.innerHTML += String.fromCharCode.apply(null, data);
     });
 
-    python.on('close', (code) => {
+    python.on('close', (data) => {
         //traces.innerHTML += '<code>child process exited with code ' + code + '</code><br>';
         //traces.innerHTML += '<code>' + code.toString('utf8') + '</code><br>';
         traces.innerHTML += String.fromCharCode.apply(null, data);
+    });
+}
+
+function runBootloader() {
+    let traces = document.getElementById('traces_boot');
+    let dircode = upath.toUnix(upath.join(__dirname, "../miner","upload-sketch.py"));
+    let python = require('child_process').spawn('python', [dircode, '']);
+    let start_boot = document.getElementById('start_boot');
+    //start_boot.setAttribute("disabled", true);
+    python.stdout.on('data', function (data) {
+        console.log("Python response: ", data.toString('utf8'));
+        //traces.innerHTML += '<code>' + data.toString('utf8') + '</code><br>';
+        traces.innerHTML += String.fromCharCode.apply(null, data);
+    });
+    python.stderr.on('data', (data) => {
+        //traces.innerHTML += '<code>child process exited with code ' + data + '</code><br>';
+        //traces.innerHTML += '<code>' + data.toString('utf8') + '</code><br>';
+        traces.innerHTML += String.fromCharCode.apply(null, data);
+    });
+    python.on('close', (data) => {
+        traces.innerHTML += 'child process exited with code ' + data;
+        //traces.innerHTML += '<code>' + code.toString('utf8') + '</code><br>';
+        // traces.innerHTML += String.fromCharCode.apply(null, data);
     });
 }
---
+++ miner/upload-sketch.py
@@ -0,0 +1,60 @@
+import sys
+import subprocess
+from configparser import ConfigParser
+
+MINER_VER = '2.49'  # Version number
+RESOURCES_DIR = 'AVRMiner_' + str(MINER_VER) + '_resources'
+
+config = ConfigParser()
+
+print('Checking for arduino-cli…')
+try:
+    subprocess.call(['arduino-cli'], shell=True)
+except Exception as e:
+    print (e)
+    print('arduino-cli is not installed. Please follow this tutorial https://siytek.com/arduino-cli-raspberry-pi/ and then try the script again, or visite https://arduino.github.io/arduino-cli/latest/installation/ and https://www.youtube.com/watch?v=1jMWsFER-Bc.')
+    sys.exit()
+
+try:
+    config.read(RESOURCES_DIR + '/Miner_config.cfg')
+    avrport = config['arduminer']['avrport']
+    board = config['arduminer']['avrboard']
+    import os
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+except Exception as e:
+    print(f'Cannot get config: {e}')
+    sys.exit()
+
+try:
+    if board == 'uno':
+        script = 'arduino-cli compile --fqbn arduino:avr:uno ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+    elif board == 'nano':
+        script = 'arduino-cli compile --fqbn arduino:avr:nano ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+    elif board == 'mega':
+    	script = 'arduino-cli compile --fqbn arduino:avr:mega ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+    elif board == 'due':
+        script = 'arduino-cli compile --fqbn arduino:avr:due ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+    else:
+    	print('no board selected')
+    subprocess.call(script, shell=True)
+except Exception as e:
+    print(f'Cannot compile: {e}')
+    sys.exit()
+
+ports = avrport.split(',')
+for port in ports:
+    try:
+        if board == 'uno':
+            upload = f'arduino-cli upload -p ' + port + ' --fqbn arduino:avr:uno ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+        elif board == 'nano':
+            upload = f'arduino-cli upload -p ' + port + ' --fqbn arduino:avr:nano ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+        elif board == 'mega':
+            upload = f'arduino-cli upload -p ' + port + ' --fqbn arduino:avr:mega ' + BASE_DIR + '/Arduino_Code/Arduino_Code.ino'
+        elif board == 'due':
+            upload = f'arduino-cli upload -p ' + port + ' --fqbn arduino:avr:due ' + BASE_DIR + '/Arduino_Due_Code/Arduino_Due_Code.ino'
+        else:
+            print('no board selected')
+        subprocess.call(upload, shell=True)
+    except Exception as e:
+        print(f'Error uploading to {port}: {e}')
+        sys.exit()
